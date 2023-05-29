d6c88e894a7e5d76fc9f6da0295adde071f3680c
qiuzhangxi <87635760+qiuzhangxi@users.noreply.github.com>
2023-05-29 17:55:13 +0800

    chore: fixed CRLF

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,34 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "plugin:prettier/recommended"
   ],
-  "overrides": [
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint",
-    "prettier"
-  ],
-  "rules": {
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
     "prettier/prettier": "error",
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
     "react/react-in-jsx-scope": "off",
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "linebreak-style": ["warn", "windows"],
+    "linebreak-style": 0
   }
 };
